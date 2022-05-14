f8c32336feb922551fe201cd8b85778735c14405
Matvey Vyrodov <marvyrodov@mail.ru>
2022-05-14 22:02:07 +0700

    Modified main.c

--- source/chessviz/main.c
+++ source/chessviz/main.c
@@ -6,6 +6,7 @@
 #include <libchess/Input.h>
 #include <libchess/Move.h>
 #include <libchess/Table.h>
+#include <libchess/seemove.h>
 
 #define n 8
 #define step 12
@@ -14,6 +15,7 @@ int main()
 {
     int** chess;
     char* input_step;
+    int redzon = 1;
 
     chess = (int**)malloc(n * sizeof(int*));
     for (int i = 0; i < n; i++)
@@ -22,11 +24,17 @@ int main()
 
     Table(chess);
     PrintTable(chess);
-    Input(input_step);
 
+    while (redzon == 1) {
+            printf("\nEnter white and black steps: ");
+            Input(input_step);
+            redzon = see_move(input_step);
+        }
     printf("\n");
+
     for (int i = 0; i < step; i++)
         printf("%c", input_step[i]);
+
     Move(chess, input_step);
     printf("\n");
     PrintTable(chess);
